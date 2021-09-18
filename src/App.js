a3e1ee2aec2817b3bab780f166e5501be055d12b
Samir İsmayılov <smrsmylvwork@gmail.com>
2021-09-18 17:23:15 +0300

    breakingbadapi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import Header from './components/ui/Header';
+import axios from 'axios';
+import CharacterGrid from './components/characters/CharacterGrid';
+import Search from './components/ui/Search';
 import './App.css';
 
-function App() {
+
+
+
+const App = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState('')
+  
+
+  useEffect(() => {
+    const fetchItems = async () =>{
+      setIsLoading(true);
+      const response = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`);
+      console.log(response.data);
+      setItems(response.data);
+      setIsLoading(false);
+    }
+
+    fetchItems();
+    
+  }, [query]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+    <Header />
+    <Search getQuery={(q) => setQuery(q)} />
+
+    <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
 }
